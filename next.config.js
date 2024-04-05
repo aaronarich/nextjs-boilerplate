4441433f5249db62815f82ded2769890cf665ca4
Aaron A. Rich <aaronarich@users.noreply.github.com>
2024-04-05 15:50:02 +0000

    Pass headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path',
+        headers: [
+          {
+            key: ':authority:',
+            value: 'e.customeriomail.com',
+          },
+          {
+            key: ':method:',
+            value: 'GET',
+          },
+          {
+            key: ':path:',
+            value: '/:path*'
+          },
+          {
+            key: ':scheme:',
+            value: 'https'
+          }
+        ],
+      },
+    ]
+  },
   async rewrites() {
     return {
       beforeFiles: [
