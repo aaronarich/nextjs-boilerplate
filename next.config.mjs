e333f72c4898403dfcf4a25bf9ff533ef3acd8d3
Aaron A. Rich <aaronarich@users.noreply.github.com>
2024-04-05 14:52:50 +0000

    Setup rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/foo',
+          destination: '/bar'
+        },
+      ]
+    },
+  }
+
 export default nextConfig;
